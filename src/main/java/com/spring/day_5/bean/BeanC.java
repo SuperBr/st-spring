9e287bd698595894d053ec11f3b1cf7e489a899b
suberb <suberb.163.com>
2018-11-20 00:26:45 +0800

    ss

---
+++ src/main/java/com/spring/day_5/bean/BeanC.java
@@ -0,0 +1,15 @@
+package com.spring.day_5.bean;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BeanC implements InterfaceA {
+
+    public void m() {
+
+    }
+
+    public void B() {
+
+    }
+}
---
+++ src/main/java/com/spring/day_5/bean/ClassC.java
@@ -0,0 +1,11 @@
+package com.spring.day_5.bean;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassC implements InterfaceA {
+    @Override
+    public void m() {
+
+    }
+}
