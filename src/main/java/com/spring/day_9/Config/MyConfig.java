09b2a81aaca0a611ec943c1de6f625d716987495
suberb <suberb.163.com>
2018-11-22 00:39:22 +0800

    Merge branch 'master' of github.com:SuperBr/st-spring
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/com/spring/day_9/Config/MyConfig.java
@@ -0,0 +1,33 @@
+package com.spring.day_9.Config;
+
+
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Configuration
+public class MyConfig {
+
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+
+        RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
+
+
+        return requestMappingHandlerMapping;
+    }
+
+
+}
---
+++ src/main/java/com/spring/day_9/action/ActionA.java
@@ -0,0 +1,15 @@
+package com.spring.day_9.action;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ActionA {
+
+    @RequestMapping("/wel")
+    public void methodA() {
+
+
+    }
+}
